7e20f98a252668001d559cffb096ef50d7a0bb37
reshinto <terencekong2002@gmail.com>
2019-11-27 11:59:46 +0800

    Add support for github pages

--- src/components/routes/BaseRouter.js
+++ src/components/routes/BaseRouter.js
@@ -3,9 +3,13 @@ import { Route } from "react-router-dom";
 import PrivateRoute from "./PrivateRoute";
 import Home from "../pages/Home";
 
+// project name
+const pn = "shoptimize"
+
 const BaseRouter = () => (
   <>
-    <Route exact path="/" component={Home} />
+  <Route exact path="/" component={Home} />
+  <Route exact path={`/${pn}`} component={Home} />
     {/*<PrivateRoute exact path="/profile" component={Profile} />*/}
   </>
 );
