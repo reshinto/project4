f773b7ca67491f9bbe8eee7e2bdef7fd677373f9
“woofborn” <wolf.born@gmail.com>
2019-11-28 13:55:42 +0800

    Landing page and draft grocery json

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,7 +1,80 @@
 import React from "react";
+// nodejs library that concatenates classes
+import classNames from "classnames";
+// @material-ui/core components
+import { makeStyles } from "@material-ui/core/styles";
 
-const Home = () => (
-  <>Home Page</>
-);
+// @material-ui/icons
 
-export default Home;
+// core components
+import Header from "../Header/Header.js";
+import Footer from "../Footer/Footer.js";
+import GridContainer from "../Grid/GridContainer.js";
+import GridItem from "../Grid/GridItem.js";
+import Button from "../CustomButtons/Button.js";
+import HeaderLinks from "../Header/HeaderLinks.js";
+import Parallax from "../Parallax/Parallax.js";
+
+import styles from "../../assets/jss/material-kit-react/views/landingPage.js";
+
+// Sections for this page
+import ProductSection from "./Sections/ProductSection.js";
+import TeamSection from "./Sections/TeamSection.js";
+import WorkSection from "./Sections/WorkSection.js";
+
+const dashboardRoutes = [];
+
+const useStyles = makeStyles(styles);
+
+export default function LandingPage(props) {
+  const classes = useStyles();
+  const { ...rest } = props;
+  return (
+    <div>
+      <Header
+        color="transparent"
+        routes={dashboardRoutes}
+        brand="shoptimize"
+
+        fixed
+        changeColorOnScroll={{
+          height: 400,
+          color: "white"
+        }}
+        {...rest}
+      />
+      <Parallax filter image="https://townsquare.media/site/341/files/2016/01/Cat.jpg?w=980&q=75">
+        <div className={classes.container}>
+          <GridContainer>
+            <GridItem xs={12} sm={12} md={6}>
+              <h1 className={classes.title}>Get your groceries and get out.</h1>
+              <h4>
+                Your guests are showing up in an hour. You have a midnight deadline. You forgot to cook dinner for date night. Whatever the reason, you need to grab what you need and go.
+                Shoptimize finds you the most efficient path to get all the items you need and get out the door ASAP.
+              </h4>
+              <br />
+              <Button
+                color="danger"
+                size="lg"
+                href="https://www.youtube.com/watch?v=dQw4w9WgXcQ&ref=creativetim"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <i className="fas fa-play" />
+                Watch video
+              </Button>
+            </GridItem>
+          </GridContainer>
+        </div>
+      </Parallax>
+      <div className={classNames(classes.main, classes.mainRaised)}>
+        <div className={classes.container}>
+
+          <TeamSection />
+
+        </div>
+      </div>
+      <Footer />
+    </div>
+  );
+}
