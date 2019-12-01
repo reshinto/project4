b0eaafbc0c10b4c928ed37421b5859b1121a5fc3
reshinto <terencekong2002@gmail.com>
2019-12-02 00:36:44 +0800

    Add form and methods from redux

--- src/components/home-components/FormDialog/Login.js
+++ src/components/home-components/FormDialog/Login.js
@@ -6,10 +6,14 @@ import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
 import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
+import { connect } from 'react-redux';
+import { login } from "../../../redux/actions/authAction";
 
-export default class LogIn extends React.Component {
+class LogIn extends React.Component {
   state = {
     open: false,
+    email: "",
+    password: ""
   };
 
   handleClickOpen = () => {
@@ -20,7 +24,30 @@ export default class LogIn extends React.Component {
     this.setState({ open: false });
   };
 
+  setStateAsync(state) {
+    return new Promise((resolve) => {
+      this.setState(state, resolve);
+    });
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    const {email, password} = this.state;
+    const userData = {
+      email,
+      password,
+    };
+    this.props.login(userData);
+  };
+
+  handleChange = async (event) => {
+    await this.setStateAsync({
+      [event.target.name]: event.target.value
+    });
+  };
+
   render() {
+    const { email, password } = this.state;
     return (
       <div>
         <Button variant="outlined" color="secondary" onClick={this.handleClickOpen}>
@@ -31,39 +58,60 @@ export default class LogIn extends React.Component {
           onClose={this.handleClose}
           aria-labelledby="form-dialog-title"
         >
-          <DialogTitle id="form-dialog-title">Log in!</DialogTitle>
-          <DialogContent>
-            <DialogContentText>
-              Great to see you again!. Please input your log in details below.
-            </DialogContentText>
-            <TextField
-              autoFocus
-              margin="dense"
-              id="name"
-              label="Email Address"
-              type="email"
-              fullWidth
-              color="secondary"
-            />
+          <form onSubmit={this.handleSubmit}>
+            <DialogTitle id="form-dialog-title">Log in!</DialogTitle>
+            <DialogContent>
+              <DialogContentText>
+                Great to see you again!. Please input your log in details below.
+              </DialogContentText>
               <TextField
-              margin="dense"
-              id="password"
-              label="Password"
-              type="password"
-              fullWidth
-              color="secondary"
-            />
-          </DialogContent>
-          <DialogActions>
-            <Button onClick={this.handleClose} color="secondary">
-              Cancel
-            </Button>
-            <Button onClick={this.handleClose} color="secondary">
-              Log in
-            </Button>
-          </DialogActions>
+                autoFocus
+                margin="dense"
+                id="name"
+                name="email"
+                label="Email Address"
+                type="email"
+                value={email}
+                onChange={this.handleChange}
+                fullWidth
+                color="secondary"
+              />
+                <TextField
+                margin="dense"
+                id="password"
+                name="password"
+                label="Password"
+                type="password"
+                value={password}
+                onChange={this.handleChange}
+                fullWidth
+                color="secondary"
+              />
+            </DialogContent>
+            <DialogActions>
+              <Button onClick={this.handleClose} color="secondary">
+                Cancel
+              </Button>
+              <Button
+                type="submit"
+                onClick={this.handleClose}
+                color="secondary"
+              >
+                Log in
+              </Button>
+            </DialogActions>
+          </form>
         </Dialog>
       </div>
     );
   }
 }
+
+const mapActionsToProps = {
+  login
+};
+
+export default connect(
+  null,
+  mapActionsToProps
+)(LogIn);
--- src/components/home-components/FormDialog/Register.js
+++ src/components/home-components/FormDialog/Register.js
@@ -6,10 +6,15 @@ import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
 import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
+import { signup } from "../../../redux/actions/authAction";
+import { connect } from "react-redux";
 
-export default class Register extends React.Component {
+class Register extends React.Component {
   state = {
     open: false,
+    username: "",
+    email: "",
+    password: ""
   };
 
   handleClickOpen = () => {
@@ -20,7 +25,32 @@ export default class Register extends React.Component {
     this.setState({ open: false });
   };
 
+  setStateAsync(state) {
+    return new Promise((resolve) => {
+      this.setState(state, resolve);
+    });
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    const {username, email, password} = this.state;
+    const newUserData = {
+      email,
+      password,
+      confirmPassword: password,
+      handle: username
+    };
+    this.props.signup(newUserData);
+  };
+
+  handleChange = async (event) => {
+    await this.setStateAsync({
+      [event.target.name]: event.target.value
+    });
+  };
+
   render() {
+    const { username, email, password } = this.state;
     return (
       <div>
         <Button color="danger"
@@ -33,44 +63,65 @@ export default class Register extends React.Component {
           onClose={this.handleClose}
           aria-labelledby="form-dialog-title"
         >
-          <DialogTitle id="form-dialog-title">Register!</DialogTitle>
-          <DialogContent>
-            <DialogContentText>
-              Welcome!. Thank you for signing up with us. :)
-            </DialogContentText>
-             <TextField
-              autoFocus
-              margin="dense"
-              id="name"
-              label="Username"
-              type="text"
-              fullWidth
-            />
-            <TextField
-              margin="dense"
-              id="email"
-              label="Email Address"
-              type="email"
-              fullWidth
-            />
+          <form onSubmit={this.handleSubmit}>
+            <DialogTitle id="form-dialog-title">Register!</DialogTitle>
+            <DialogContent>
+              <DialogContentText>
+                Welcome!. Thank you for signing up with us. :)
+              </DialogContentText>
+               <TextField
+                autoFocus
+                margin="dense"
+                id="name"
+                name="username"
+                label="Username"
+                type="text"
+                value={username}
+                onChange={this.handleChange}
+                fullWidth
+              />
               <TextField
-              margin="dense"
-              id="password"
-              label="Password"
-              type="password"
-              fullWidth
-            />
-          </DialogContent>
-          <DialogActions>
-            <Button onClick={this.handleClose} color="primary">
-              Cancel
-            </Button>
-            <Button onClick={this.handleClose} color="primary">
-              Register
-            </Button>
-          </DialogActions>
+                margin="dense"
+                id="email"
+                name="email"
+                label="Email Address"
+                type="email"
+                value={this.state.email}
+                onChange={this.handleChange}
+                fullWidth
+              />
+              <TextField
+                margin="dense"
+                id="password"
+                name="password"
+                label="Password"
+                type="password"
+                value={password}
+                onChange={this.handleChange}
+                fullWidth
+              />
+            </DialogContent>
+            <DialogActions>
+              <Button onClick={this.handleClose} color="primary">
+                Cancel
+              </Button>
+              <Button
+                type="submit"
+                onClick={this.handleClose}
+                color="primary"
+              >
+                Register
+              </Button>
+            </DialogActions>
+          </form>
         </Dialog>
       </div>
     );
   }
 }
+
+const mapDispatchToProps = {
+  signup,
+};
+
+export default connect(null, mapDispatchToProps)(Register)
