dbb8c552e5e27f912b2c6eb12c5c68667d8ef25e
“woofborn” <wolf.born@gmail.com>
2019-12-03 15:50:12 +0800

    All item cards have suitable pictures

--- src/components/pages/Search.js
+++ src/components/pages/Search.js
@@ -40,19 +40,19 @@ const allItems =
                         "aisle":1,
                         "items":[
                             {name:"fish",
-                            img: "../search-components/apples.png"
+                            img: "https://www.thespruceeats.com/thmb/yPLWl_gyHEd0479KtY8NxoMNspM=/960x0/filters:no_upscale():max_bytes(150000):strip_icc()/148964914-56a2f6f35f9b58b7d0cfe462.jpg"
                             },
                             {name:"dumplings",
-                            img: "../search-components/apples.png"
+                            img: "https://static01.nyt.com/images/2014/07/29/magazine/Dumplings1/Dumplings1-tmagArticle.jpg"
                             },
                             {name:"pizza",
-                            img: "../search-components/apples.png"
+                            img: "http://www.marketreportgazette.com/wp-content/uploads/2019/08/Frozen-Pizza.jpg"
                             },
                             {name:"prata",
-                            img: "../search-components/prata.png"
+                            img: "https://d22ir9aoo7cbf6.cloudfront.net/wp-content/uploads/sites/2/2016/03/roti-prata-in-singapore.jpg"
                             },
                             {name:"ice cream",
-                            img: "../search-components/watermelon.png"
+                            img: "https://previews.123rf.com/images/foodandmore/foodandmore1404/foodandmore140400039/27128938-array-of-different-flavored-colorful-ice-cream-in-plastic-tubs-displayed-on-an-old-wooden-table-at-a.jpg"
                             }]
                     },
 
@@ -62,17 +62,17 @@ const allItems =
                             {name:"apples",
                             img: "https://i1.wp.com/4f7bg01yl3z03jojs41hgo8k-wpengine.netdna-ssl.com/wp-content/uploads/2016/10/HF160920_Global_Blog_All_About_Apples_15_low.jpg?ssl=1"
                             },
-                            {name:"pears",
-                            img: "https://placekitten.com/400/300"
-                            },
                             {name:"blueberries",
-                            img: "https://placekitten.com/400/300"
+                            img: "https://i0.wp.com/images-prod.healthline.com/hlcmsresource/images/AN_images/blueberries-1296x728-feature.jpg?w=1155&h=1528"
+                            },
+                            {name:"pears",
+                            img: "https://www.stemilt.com/wp-content/uploads/2016/07/Concorde.jpg"
                             },
                             {name:"strawberries",
-                            img: "https://placekitten.com/400/300"
+                            img: "https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fcdn-image.myrecipes.com%2Fsites%2Fdefault%2Ffiles%2Fstyles%2Fmedium_2x%2Fpublic%2Ffield%2Fimage%2Fstrawberry-calories-hero-getty.jpg%3Fitok%3DgMhqclgZ&w=450&c=sc&poi=face&q=85"
                             },
                             {name:"watermelons",
-                            img: "https://media1.tenor.com/images/20a169a15eee8f97fdb0e540bacb5cbd/tenor.gif?itemid=14510497"
+                            img: "https://media.fromthegrapevine.com/assets/images/2016/7/watermelon-whole-one-half.jpg.480x0_q71_crop-scale.jpg"
                             }
                        ]
                     },
@@ -81,57 +81,57 @@ const allItems =
                         "aisle": 3,
                         "items":[
                             {name:"cucumbers",
-                            img: "../search-components/apples.png"
+                            img: "https://cfig.ca/wp-content/uploads/2018/11/cucumber.jpg"
                             },
                             {name:"carrots",
-                            img: "../search-components/apples.png"
+                            img: "https://hips.hearstapps.com/ghk.h-cdn.co/assets/18/09/2048x1364/gallery-1519672422-carrots.jpg?resize=480:*"
                             },
                             {name:"onions",
-                            img: "../search-components/apples.png"
+                            img: "https://www.thespruceeats.com/thmb/sxx0JHQRPnPV0heagzJFz__zarE=/960x0/filters:no_upscale():max_bytes(150000):strip_icc()/onions-2500-5839b12d5f9b58d5b1107a90.jpg"
                             },
                             {name:"capsicum",
-                            img: "../search-components/strawberries.png"
+                            img: "https://1v1d1e1lmiki1lgcvx32p49h8fe-wpengine.netdna-ssl.com/wp-content/uploads/2018/04/1522916196-coloured-capsicums-peppers-.jpg"
                             },
                             {name:"celery",
-                            img: "../search-components/watermelon.png"
+                            img: "https://cdn.apartmenttherapy.info/image/fetch/f_auto,q_auto:eco,c_fill,g_auto,w_1500/https%3A%2F%2Fstorage.googleapis.com%2Fgen-atmedia%2F3%2F2014%2F10%2F1c340c24e50e2a71609db0d2c0d77849b36ba2b4.jpeg"
                             }]
                     },
                     "snacks and candy":{
                         "aisle":4,
                         "items":[
                             {name:"chocolate bars",
-                            img: "../search-components/apples.png"
+                            img: "https://cdn-a.william-reed.com/var/wrbm_gb_food_pharma/storage/images/publications/food-beverage-nutrition/confectionerynews.com/article/2019/08/19/new-study-finds-steep-rise-in-sugar-levels-in-uk-chocolate-bars/10055756-1-eng-GB/New-study-finds-steep-rise-in-sugar-levels-in-UK-chocolate-bars_wrbm_large.jpg"
                             },
                             {name:"potato chips",
-                            img: "../search-components/apples.png"
+                            img: "https://previews.123rf.com/images/kwangmoo/kwangmoo1807/kwangmoo180700317/104709437-crispy-potato-chips-snack-texture-background.jpg"
                             },
                             {name:"nuts",
-                            img: "../search-components/apples.png"
+                            img: "https://media.npr.org/assets/img/2019/09/27/nuts-1_custom-61cfca772f4f991e157977ffe42febcb8c23d7dc-s800-c85.jpg"
                             },
                             {name:"gummies",
-                            img: "../search-components/strawberries.png"
+                            img: "https://tidbitsmag.com/wp-content/uploads/2016/08/20685982-Assortment-of-gummies-on-a-market-stall-Stock-Photo.jpg"
                             },
                             {name:"seaweed",
-                            img: "../search-components/watermelon.png"
+                            img: "https://media.npr.org/assets/img/2017/08/31/istock-605786284-34f5b78a564c2a7c6e7bfe5d68a2b830a42e4238-s800-c85.jpg"
                             }]
                     },
                     "baking needs": {
                         "aisle":5,
                         "items": [
                             {name:"flour",
-                            img: "../search-components/apples.png"
+                            img: "https://img2.exportersindia.com/product_images/bc-full/2019/5/6121355/whole-wheat-flour-1559192789-4929971.jpeg"
                             },
                             {name:"baking chocolate",
-                            img: "../search-components/apples.png"
+                            img: "https://cdn.sallysbakingaddiction.com/wp-content/uploads/2017/02/baking-with-chocolate-5.jpg"
                             },
                             {name:"icing",
-                            img: "../search-components/apples.png"
+                            img: "https://www.biggerbolderbaking.com/wp-content/uploads/2018/03/BBB214-Crazy-Frosting-Website-Featured-Image.jpg"
                             },
                             {name:"baking soda",
-                            img: "../search-components/strawberries.png"
+                            img: "https://www.thespruceeats.com/thmb/e8HKZPTx6Q6GxiCHcudw9QrAHC8=/960x0/filters:no_upscale():max_bytes(150000):strip_icc()/baking-soda-58ace6af5f9b58a3c9b23ab2.jpg"
                             },
                             {name:"pancake mix",
-                            img: "../search-components/watermelon.png"
+                            img: "https://food.fnr.sndimg.com/content/dam/images/food/fullset/2019/7/11/0/FNK_the-best-pancakes_H_s4x3.jpg.rend.hgtvcom.826.620.suffix/1562853903667.jpeg"
                             }]
                     }
 
@@ -267,7 +267,7 @@ export default class Search extends React.Component {
 
       <ResultsCategory allData = {allItems}  category = {this.state.selectedCategoryOption}  list = {this.addToList}/>
       <Grid container
-      style = {{borderTop:"1px solid black"}}>
+      style = {{borderTop:"1px solid black", marginTop: 10}}>
 
           <Grid item xs={6}>
             <h3 style = {{color:"rgb(156, 39, 176)"}}>Select Store</h3>
